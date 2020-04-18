dca2202d6e04a9b2a46bb337f21470ae05c92390
Nikhil Komirisetti <nikhilk@live.unc.edu>
2020-04-18 16:59:36 -0400

    updated comments

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -8,7 +8,7 @@ const setupMainPage = function () {
 
     socket.on('open-game-room', function (player) {
         setTimeout(function(){
-
+            
         }, 60000);
         $('#rootContainer').text("open game room");
     });
@@ -27,8 +27,17 @@ $(document).ready(function () {
 
 // games
 
-// as still as possible
-// tap as quickly as possible
+// as still as possible points == less movementy
+// tap as quickly as possible 
 // jump around as much as possible
 // jog in place
 // karate chop
+//      singing game
+// 
+
+// text to speech javascript  - kevin
+// using accelerometer        - nikhil
+// creating the narration (what order things are gonna be said in) - nuran
+//     - 
+// add visual elements
+// displaying narration
